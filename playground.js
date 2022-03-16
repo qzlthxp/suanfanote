ace3d5a7a7a7223f2ebe8d53a35108353ffced49
蔡承祺 <985234173@qq.com>
2022-03-16 16:46:40 +0800

    quicksort

--- playground.js
+++ playground.js
@@ -1,16 +1,14 @@
 const arr = [12, 312, 4512, 12, 124]
 
 function quickSort(arr, L, R) {
-  if (L === R) {
-    return
-  }
+  if (L > R) return
   let i = L
   let j = R
-  while (i < j) {
-    while (arr[j] < arr[L] && i < j) {
+  while (i !== j) {
+    while (arr[j] >= arr[L] && i < j) {
       j--
     }
-    while (arr[i] > arr[L] && i < j) {
+    while (arr[i] <= arr[L] && i < j) {
       i++
     }
     if (i < j) {
@@ -20,7 +18,7 @@ function quickSort(arr, L, R) {
 
   ;[arr[L], arr[i]] = [arr[i], arr[L]]
 
-  quickSort(arr, L, i)
+  quickSort(arr, L, i - 1)
   quickSort(arr, i + 1, R)
 }
 
